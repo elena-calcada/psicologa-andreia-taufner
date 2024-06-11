e64cde8298620a70af086bf5d93857b4ef46b63f
Elena Calcada Evangelista <calcada.elena@gmail.com>
2024-06-11 19:50:34 -0300

    change first message

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <div className="h-screen flex flex-col items-center justify-center bg-backgroundColor-100">
-        <Image src={"/assets/logo.svg"} width={505} height={222} alt="Logo" />
-        <h1 className="text-4xl mb-4 mt-4 text-titleColor">Site da psicóloga Anrdeia Taufner!</h1>
-        <h2 className="text-3xl">Em construção...</h2>
+      <div className="h-screen flex flex-col items-center justify-center bg-backgroundColor-100 p-4">
+        <Image src={"/assets/logo.svg"} width={360} height={158} alt="Logo" />
+        <div>
+          <h1 className="text-center text-3xl mb-4 mt-4 text-titleColor">Psicóloga Anrdeia Taufner</h1>
+          <h2 className="text-center text-2xl">Em construção...</h2>
+        </div>
       </div>
     </main>
   );
