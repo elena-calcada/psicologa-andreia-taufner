2592675e5d967b70ae8e954db3149b3ca561c7db
Elena Calcada Evangelista <calcada.elena@gmail.com>
2024-06-11 20:11:21 -0300

    changes in first message

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="h-screen flex flex-col items-center justify-center bg-backgroundColor-100 p-4">
         <Image src={"/assets/logo.svg"} width={360} height={158} alt="Logo" />
         <div>
-          <h1 className="text-center text-3xl mb-4 mt-4 text-titleColor">Psicóloga Anrdeia Taufner</h1>
+          <h1 className="text-center text-3xl mb-4 mt-4 text-titleColor">Psicóloga Andreia Taufner</h1>
           <h2 className="text-center text-2xl">Em construção...</h2>
         </div>
       </div>
