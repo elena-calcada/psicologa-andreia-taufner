b2cd01a199df53c6521119a42324d75bfb038f4f
Elena Calcada Evangelista <calcada.elena@gmail.com>
2024-06-15 19:30:19 -0300

    section home: change h1

--- components/Start.tsx
+++ components/Start.tsx
@@ -6,7 +6,7 @@ import LinkButtom from "./LinkButton"
 const Start = () => {
   return (
     <section className="h-[calc(80vh+8rem)] bg-home-mobile bg-cover bg-center bg-no-repeat sm:bg-home-desktop " id="home">
-      <div className="flex flex-col gap-10 h-full mx-auto pt-56 2xl:pt-64 px-[15px] md:px-[24px] xl:max-w-[1400px]">
+      <div className="flex flex-col gap-10 h-full mx-auto pt-56 2xl:pt-64 px-[15px] md:px-[24px] lg:px-[48px] xl:max-w-[1400px]">
         <div className="flex flex-col gap-8">
           <motion.h1
             className="h1 xs:!text-7xl xl:text-[128px]"
@@ -15,7 +15,7 @@ const Start = () => {
             whileInView={"show"}
             viewport={{ once: false, amount: 0.2 }}
           >
-            Andreia Taufner
+            Andréia Taufner
           </motion.h1>
           <motion.p
             className="p !text-titleColor !text-xl !font-medium max-w-[500px] xs:max-w-[400px] lg:max-w-[700px] lg:!text-2xl"
@@ -24,7 +24,7 @@ const Start = () => {
             whileInView={"show"}
             viewport={{ once: false, amount: 0.2 }}
           >
-            Psicóloga Clínica de crianças, adolescentes e adultos, em Vila Velha/ES -  CRP 16/09385
+            Psicóloga Clínica de crianças, adolescentes e adultos em Vila Velha/ES -  CRP 16/09385
           </motion.p>
         </div>
         <motion.div
