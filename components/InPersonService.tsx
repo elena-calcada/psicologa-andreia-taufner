a7175499c04af524f58da1515186888b1029162d
Elena Calcada Evangelista <calcada.elena@gmail.com>
2024-06-19 20:48:07 -0300

    change image configuration - online and in person

--- components/InPersonService.tsx
+++ components/InPersonService.tsx
@@ -9,7 +9,7 @@ const InPersonService = () => {
       <section className="bg-white flex items-center justify-center" id="in-person-service">
         <div className="container">
           <motion.h2
-            className="h2 !mb-24 text-center"
+            className="h2 !mb-20 text-center"
             variants={fadeIn("up", 0.4)}
             initial="hidden"
             whileInView={"show"}
