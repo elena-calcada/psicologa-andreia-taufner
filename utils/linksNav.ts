7ae70a1eb35b009335eb57733fe7e955a7c3111a
Elena Calcada Evangelista <calcada.elena@gmail.com>
2024-06-19 22:53:15 -0300

    target resizing -40

--- utils/linksNav.ts
+++ utils/linksNav.ts
@@ -2,8 +2,8 @@ export const linksNav = [
   { name: 'Início', target: 'home', offset: -100 },
   { name: 'Sobre', target: 'aboult', offset: -60 },
   { name: 'Áreas de atuação', target: 'areas', offset: -60 },
-  { name: 'Atendimento Online', target: 'online-service', offset: -50 },
-  { name: 'Atendimento Presencial', target: 'in-person-service', offset: -50 },
+  { name: 'Atendimento Online', target: 'online-service', offset: -40 },
+  { name: 'Atendimento Presencial', target: 'in-person-service', offset: -40 },
   { name: 'Consultório', target: 'clinic', offset: 0 },
   { name: 'Contato', target: 'contact', offset: 0 },
 ]
