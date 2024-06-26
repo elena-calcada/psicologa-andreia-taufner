cab3fce288f3657fc04055eed8bdbf05609dba63
Elena Calcada Evangelista <calcada.elena@gmail.com>
2024-06-25 22:22:46 -0300

    change affset

--- utils/linksNav.ts
+++ utils/linksNav.ts
@@ -2,8 +2,8 @@ export const linksNav = [
   { name: 'Início', target: 'home', offset: -100 },
   { name: 'Sobre', target: 'aboult', offset: -40 },
   { name: 'Áreas de atuação', target: 'areas', offset: -60 },
-  { name: 'Atendimento Online', target: 'online-service', offset: -20 },
-  { name: 'Atendimento Presencial', target: 'in-person-service', offset: -20 },
+  { name: 'Atendimento Online', target: 'online-service', offset: -30 },
+  { name: 'Atendimento Presencial', target: 'in-person-service', offset: -30 },
   { name: 'Consultório', target: 'clinic', offset: -40 },
   { name: 'Contato', target: 'contact', offset: -40 },
 ]
