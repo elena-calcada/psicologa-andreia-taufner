04ccfc8d1ec73d3b2c3cb8bc970c3962eebf69a8
Elena Calcada Evangelista <calcada.elena@gmail.com>
2024-06-25 21:42:49 -0300

    h2: change size

--- utils/linksNav.ts
+++ utils/linksNav.ts
@@ -1,6 +1,6 @@
 export const linksNav = [
   { name: 'Início', target: 'home', offset: -100 },
-  { name: 'Sobre', target: 'aboult', offset: -60 },
+  { name: 'Sobre', target: 'aboult', offset: -40 },
   { name: 'Áreas de atuação', target: 'areas', offset: -60 },
   { name: 'Atendimento Online', target: 'online-service', offset: -40 },
   { name: 'Atendimento Presencial', target: 'in-person-service', offset: -40 },
