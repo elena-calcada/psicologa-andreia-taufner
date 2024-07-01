7d9c3003f9d3d6a854b83c660c6c435fe23003a2
Elena Calcada Evangelista <calcada.elena@gmail.com>
2024-07-01 15:02:23 -0300

    section clinic -  change images

--- utils/clinicImages.ts
+++ utils/clinicImages.ts
@@ -1,21 +1,26 @@
 export const clinicImages = [
   {
     image: "/assets/clinic/image1.png",
-    alt: "",
+    alt: "Uma sala pequena e aconchegante com duas cadeiras estofadas na cor cinza, uma delas com almofadas de tecido estampado com detalhes verdes e brancos. A sala tem uma parede branca com um desenho de arabescos verde-claro e um tapete marrom com desenhos circulares.",
   },
 
   {
     image: "/assets/clinic/image2.png",
-    alt: "",
+    alt: "Uma sala pequena e aconchegante com duas cadeiras estofadas azuis, uma mesa de centro com um livro e uma xícara de café, um tapete branco e uma televisão pendurada na parede branca.",
   },
 
   {
     image: "/assets/clinic/image3.png",
-    alt: "",
+    alt: "Uma sala pequena com duas cadeiras estofadas verdes com almofadas verdes, uma cortina verde atrás das cadeiras, um tapete branco, uma mesa de centro com um livro e um vaso com flores. A parede branca possui um quadro com um desenho de ramos e uma folha.",
   },
 
   {
     image: "/assets/clinic/image4.png",
-    alt: "",
-  }
+    alt: "Uma sala com uma mesa de madeira escura e duas cadeiras verdes, com cortinas verdes na parede atrás da mesa. Há uma prateleira de madeira na parede à direita com diversos objetos coloridos, e três círculos com borboletas azuis em um círculo verde na parede à esquerda.",
+  },
+
+  {
+    image: "/assets/clinic/image5.png",
+    alt: "Uma sala com uma mesa de madeira escura e duas cadeiras verdes, com cortinas verdes na parede atrás da mesa. Há uma prateleira de madeira na parede à direita com diversos objetos coloridos, e três círculos com borboletas azuis em um círculo verde na parede à esquerda.",
+  },
 ]
